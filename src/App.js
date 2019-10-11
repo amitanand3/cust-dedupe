6da28be5f072402c5368a4febf8057e3a0614200
Amit Anand <amit.anand@publicissapient.com>
2019-10-11 08:19:44 +0530

    added xml generation

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import UserDetailsComp from './Property/UserDetailsComp';
 
 class App extends Component {
 
+  propList = ['First Name', 'Middle Name', 'Last Name', 'Date of Birth', 'Phone', 'Zipcode'];
+
   toggleView = (event) => {
     event.currentTarget.classList.toggle("active");
     let panel = event.currentTarget.nextElementSibling;
@@ -15,33 +17,47 @@ class App extends Component {
     }
   }
 
+  resetForm = () => {
+    document.getElementById("prop-form").reset();
+    document.getElementById("val-form").reset();
+  }
+
   createXML = () => {
     let xmlDoc = document.implementation.createDocument("", "", null);
     let rootElem = xmlDoc.createElement("duke");
     let schema = xmlDoc.createElement("schema");
     
-    let property = xmlDoc.createElement("property");
-    property.setAttribute("lookup", "true");
+    this.propList.forEach((prop, index) => {
+      let form = document.property[index];
+      let property = xmlDoc.createElement("property");
+      property.setAttribute("lookup", form.elements["lookup"].checked);
+  
+      let name = xmlDoc.createElement("name");
+      name.innerHTML = prop;
+      let comp = xmlDoc.createElement("comparator");
+      comp.innerHTML = form.elements["comp"].value;
+      let low = xmlDoc.createElement("low");
+      low.innerHTML = form.elements["low"].value || "0.5";
+      let high = xmlDoc.createElement("high");
+      high.innerHTML = form.elements["high"].value || "0.9";
 
-    let name = xmlDoc.createElement("name");
-    name.innerHTML = "firstName";
-    let comp = xmlDoc.createElement("comparator");
-    comp.innerHTML = "no.priv.garshol.duke.comparators.JaroWinklerTokenized";
-    let low = xmlDoc.createElement("low");
-    low.innerHTML = "0.5";
-    let high = xmlDoc.createElement("high");
-    high.innerHTML = "0.9";
+      property.appendChild(name);
+      property.appendChild(comp);
+      property.appendChild(low);
+      property.appendChild(high);
 
-    property.appendChild(name);
-    property.appendChild(comp);
-    property.appendChild(low);
-    property.appendChild(high);
+      schema.appendChild(property);
+    });
 
-    schema.appendChild(property);
     rootElem.appendChild(schema);
     xmlDoc.appendChild(rootElem);
 
     console.log(xmlDoc);
+    let xmlSerializer = new XMLSerializer();
+    let xmlString = xmlSerializer.serializeToString(xmlDoc);
+    console.log(xmlString);
+    let tempTab = window.open("");
+    tempTab.document.write('<textarea style="border:none; -webkit-box-sizing: border-box; -moz-box-sizing: border-box;         box-sizing: border-box; width:100%; height:100%">'+xmlString+'</textarea>');
   }
   
   render() {
@@ -53,14 +69,13 @@ class App extends Component {
               <u>Customer Master Deduplication Portal</u>
             </h1>
           </legend>
-          <UserDetailsComp toggle={(event) => this.toggleView(event)} propertyName={'First Name'} />
-          <UserDetailsComp toggle={(event) => this.toggleView(event)} propertyName={'Middle Name'} />
-          <UserDetailsComp toggle={(event) => this.toggleView(event)} propertyName={'Last Name'} />
-          <UserDetailsComp toggle={(event) => this.toggleView(event)} propertyName={'Date of Birth'} />
-          <UserDetailsComp toggle={(event) => this.toggleView(event)} propertyName={'Phone'} />
-          <UserDetailsComp toggle={(event) => this.toggleView(event)} propertyName={'Zipcode'} />
+          {
+            this.propList.map((value, index) => {
+              return <UserDetailsComp key={index} toggle={(event) => this.toggleView(event)} propertyName={value} />
+            })
+          }          
           <div className="center">
-            <button className="m-10 bg-coral">RESET</button>
+            <button className="m-10 bg-coral" onClick={()=>this.resetForm()}>RESET</button>
             <button className="m-10 bg-lgreen" onClick={()=>this.createXML()}>SUBMIT</button>
           </div>
         </fieldset>
--- src/Property/UserDetailsComp.js
+++ src/Property/UserDetailsComp.js
@@ -6,53 +6,57 @@ const UserDetailsComp = (props) => {
         <div>
             <button className="accordion" onClick={props.toggle}>{props.propertyName}</button>
             <div className="panel" style={{"maxHeight": "290px"}}>
-                <fieldset className="align-left">
-                <legend>Property Selector</legend>
-                <div>
-                    <label><input type="radio" />Lookup</label>
-                </div>
-                <div>
-                    <label>Comparator</label>
-                    <select className="w-270">
-                    <option>Jaro Winkler Tokenized</option>
-                    <option>Levenshtein</option>
-                    <option>Exact Comparator</option>
-                    <option>Weighted Levenshtein</option>
-                    <option>Numeric Comparator</option>
-                    </select>
-                </div>
-                <div>
-                    <label>Low</label>
-                    <input type="number" min="0.0" max="0.5" step="0.1" placeholder="Enter value between 0 to 0.5 (Default: 0.5)" className="w-270" />
-                </div>
-                <div>
-                    <label>High</label>
-                    <input type="number" min="0.5" max="1.0" step="0.1" placeholder="Enter value between 0.5 to 1 (Default: 0.9)" className="w-270" />
-                </div>
-                </fieldset>
-                <fieldset>
-                <legend>Values</legend>
-                <div>
-                    <label>Threshold</label>
-                    <input type="number" className="w-310" placeholder="Default: 0.9" />
-                </div>
-                <div>
-                    <label>Max Search Hits</label>
-                    <input type="number" className="w-310" placeholder="Default: 10" />
-                </div>
-                <div>
-                    <label>Min Relevance</label>
-                    <input type="number" className="w-310" placeholder="Default: 0.9" />
-                </div>
-                <div>
-                    <label>Fuzzy Search</label>
-                    <input type="text" className="w-310" placeholder="True/False (Default: True)" />
-                </div>
-                <div>
-                    <label>Path</label>
-                    <input type="text" className="w-310" placeholder="Default drive path for lucene index (/opt/flink/lucene)" />
-                </div>
-                </fieldset>
+                <form id="prop-form" name="property">
+                    <fieldset className="align-left">
+                    <legend>Property Selector</legend>
+                    <div>
+                        <label><input type="radio" name="lookup" />Lookup</label>
+                    </div>
+                    <div>
+                        <label>Comparator</label>
+                        <select name="comp" className="w-270">
+                            <option value="Jaro Winkler Tokenized">Jaro Winkler Tokenized</option>
+                            <option value="Levenshtein">Levenshtein</option>
+                            <option value="Exact Comparator">Exact Comparator</option>
+                            <option value="Weighted Levenshtein">Weighted Levenshtein</option>
+                            <option value="Numeric Comparator">Numeric Comparator</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label>Low</label>
+                        <input name="low" type="number" min="0.0" max="0.5" step="0.1" placeholder="Enter value between 0 to 0.5 (Default: 0.5)" className="w-270" />
+                    </div>
+                    <div>
+                        <label>High</label>
+                        <input name="high" type="number" min="0.5" max="1.0" step="0.1" placeholder="Enter value between 0.5 to 1 (Default: 0.9)" className="w-270" />
+                    </div>
+                    </fieldset>
+                </form>
+                <form id="val-form" name="values">
+                    <fieldset>
+                    <legend>Values</legend>
+                    <div>
+                        <label>Threshold</label>
+                        <input name="threshold" type="number" className="w-310" placeholder="Default: 0.9" />
+                    </div>
+                    <div>
+                        <label>Max Search Hits</label>
+                        <input name="hits" type="number" className="w-310" placeholder="Default: 10" />
+                    </div>
+                    <div>
+                        <label>Min Relevance</label>
+                        <input name="minRel" type="number" className="w-310" placeholder="Default: 0.9" />
+                    </div>
+                    <div>
+                        <label>Fuzzy Search</label>
+                        <input name="fuzzy" type="text" className="w-310" placeholder="True/False (Default: True)" />
+                    </div>
+                    <div>
+                        <label>Path</label>
+                        <input name="path" type="text" className="w-310" placeholder="Default drive path for lucene index (/opt/flink/lucene)" />
+                    </div>
+                    </fieldset>
+                </form>
             </div>
       </div>
     );
